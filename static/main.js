3a870f311c3e2542277b7aac0be8c6f7057f7fd9
openpyone <open.pyone@gmail.com>
2022-08-29 17:33:40 +0800

    modified Jitsi rooms

--- static/main.js
+++ static/main.js
@@ -11,7 +11,7 @@ $(function() {
 
 // hide menu after click menu item
 $(document).on('click','.navbar-collapse',function(e) {
-    if( $(e.target).is('a') && ( $(e.target).attr('class') != 'dropdown-toggle' ) ) {
+    if( $(e.target).is('a') && ( $(e.target).attr('class').indexOf('dropdown-toggle') == -1) ) {
         $(this).collapse('hide');
     }
 });
